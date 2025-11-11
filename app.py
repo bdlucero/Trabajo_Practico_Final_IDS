046e0439af2eac1c8fc38112e34c2ecaedf61b58
Beatriz Romero <bromero@fi.uba.ar>
2025-11-11 10:54:31 -0300

    Se modifica el app.py incorporando las rutas de los errores 404 y 500, y la ruta al template resenas.html

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ GOOGLE_CLIENT_ID = "469004002801-logurlhvbb0e682h0rfesar7vtl6f0o0.apps.googleuse
 
 # ===== RUTAS =====
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error404.html'),404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('error500.html'),500
+
 @app.route("/")
 def home():
     return redirect(url_for("buscar"))
@@ -44,6 +52,10 @@ def registro():
 def logout():
     session.clear()
     return redirect(url_for("buscar"))
+
+@app.route("/reseñas")
+def resenas():
+    return render_template("resenas.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
