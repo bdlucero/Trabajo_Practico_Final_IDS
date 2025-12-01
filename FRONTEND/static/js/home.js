b76eeb996da55b0c4a2f25ff029fba59477688ed
Felipe <forellano@fi.uab.ar>
2025-12-01 00:36:17 +0000

    Se producen cambios en home.js y home .html

--- FRONTEND/static/js/home.js
+++ FRONTEND/static/js/home.js
@@ -6,25 +6,11 @@ function rechazarDatos() {
     mostrarToast("No se usarán tus datos personales.");
 }
 
-function mostrarToast(mensaje) {
-    const toast = document.getElementById("popup-confirmacion");
-    toast.textContent = mensaje;
-    toast.classList.add("show");
-    setTimeout(() => toast.classList.remove("show"), 3000);
-}
-
-document.getElementById("btn-lupa").onclick = function() {
-    const input = document.getElementById("input-busqueda");
-    input.style.display = input.style.display === "none" ? "inline-block" : "none";
-    input.focus();
-};
 
 function mostrarOpciones() {
     const opciones = document.getElementById("opciones-privacidad");
     opciones.style.display = opciones.style.display === "none" ? "block" : "none";
 }
-//                     //
-
 
 function confirmarBloque(idBloque, acepta) {
     const bloque = document.getElementById(idBloque);
